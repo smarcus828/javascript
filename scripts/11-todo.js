adfd08f6ce0973a103c40ad3e07aa0080149044d
Steven Marcus <smarcus@cicsnc.org>
2023-10-10 07:05:50 -0400

    leaving for work

--- scripts/11-todo.js
+++ scripts/11-todo.js
@@ -1,20 +1,27 @@
 const todoList = [];
 let HTMLlist = '';
 
-function addToDo(newThing) {
-  todoList.push(newThing);
-  console.log(todoList);
+function addToDo(newThing, newdate) {
+  todoList.push({name: newThing, dueDate: newdate});
+  // console.log(todoList);
 }
 
 function displayList() {
   HTMLlist='';
   for (let i = 0; i < todoList.length; i++){
-    HTMLlist += `<p>${todoList[i]} 
-      <button 
+    const name = todoList[i].name;
+    const due = todoList[i].dueDate;
+    HTMLlist += `<p>
+      ${name}
+      ${due} 
+      <button
+        onclick="todoList.splice(${i},1);
+        displayList();"
         class="stdbutton deletebutton"
-      > Delete </button><p>`
+        > Delete </button>
+      <p>`
   }
-  console.log(todoList)
-  console.log(HTMLlist);
+  // console.log(todoList)
+  // console.log(HTMLlist);
   document.getElementById('listdisplay').innerHTML = HTMLlist;
 }
