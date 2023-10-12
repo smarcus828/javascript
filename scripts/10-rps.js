5672412165bd4d1654ef8e855ef16bfd0bd6b76f
Steven Marcus <smarcus@cicsnc.org>
2023-10-11 21:03:03 -0400

    done with rps

--- scripts/10-rps.js
+++ scripts/10-rps.js
@@ -1,3 +1,5 @@
+let playing = false;
+
 const tally = {
   wins: 0,
   losses: 0,
@@ -19,26 +21,26 @@ const tally = {
   }
 }
 
+function getPlay(){
+    const Num = Math.random(); 
+    if (Num < 1/3) {
+        return('ROCK');
+    } else if ((Num > 1/3) && (Num < 2/3)) {
+        return('PAPER');
+    } else {
+        return('SCISSORS');
+    }
+}
+
+
 function playGame(userChoiceInput) {
-  const compNum = Math.random();
   let compChoice = 'none';
   let result = 'none';
 
-  console.log(`userChoice: ${userChoiceInput} and
-  compNum: ${compNum}`);
-
-  // Find Computer Choice
-  if (compNum < 1/3) {
-      compChoice = 'ROCK';
-  } else if ((compNum > 1/3) && (compNum < 2/3)) {
-      compChoice = 'PAPER';
-  } else {
-      compChoice = 'SCISSORS';
-  }
-
-  console.log(`compChoice: ${compChoice}`);
+  compChoice = getPlay();
 
   // Decide Winner
+  // ROCK
   if (userChoiceInput == 'ROCK'){
       if (compChoice === 'PAPER'){
           result = 'LOSS';
@@ -50,9 +52,11 @@ function playGame(userChoiceInput) {
           result = 'TIE';
           tally.addTie();
       }
+      displayWin(userChoiceInput, compChoice, result);
 
   }
 
+  // PAPER
   if (userChoiceInput == 'PAPER'){
       if (compChoice === 'SCISSORS'){
           result = 'LOSS';
@@ -64,9 +68,11 @@ function playGame(userChoiceInput) {
           result = 'TIE';
           tally.addTie();
       }
+      displayWin(userChoiceInput, compChoice, result);
 
   }
 
+  // SCISSORS
   if (userChoiceInput == 'SCISSORS'){
       if (compChoice === 'ROCK'){
           result = 'LOSS';
@@ -78,9 +84,37 @@ function playGame(userChoiceInput) {
           result = 'TIE';
           tally.addTie();
       }
+      displayWin(userChoiceInput, compChoice, result);
 
   }
   console.log(`Result: ${result}`);
   console.log(`Wins: ${tally.wins}\nLosses: ${tally.losses}\nTies: ${tally.ties}`)
+  displayTally();
+}
+
+function playAlone(){
+    if (!playing){
+        intervalid=setInterval(function () { playGame(getPlay())},200);
+        playing = true;
+    } else {
+        clearInterval(intervalid);
+        playing=false;
+    }
+}
+
+function displayWin(human,computer,result){
+    document.getElementById('winPanel')
+    .innerHTML = `
+    <div>Human: ${human}</div>
+    <div>Computer: ${computer}</div> 
+    <div>Result: ${result} </div>`;
+}
+
+function displayTally(){
+    document.getElementById('resultPanel')
+    .innerHTML = `
+    <div>Wins: ${tally.wins} </div>
+    <div>Losses: ${tally.losses} </div>
+    <div>Ties: ${tally.ties} </div>`;
 }
 
