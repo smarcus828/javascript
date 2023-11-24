339cb615b40bb4bd7a2fb86c28745d18dadb94bf
Steven Marcus <smarcus@cicsnc.org>
2023-11-24 11:28:24 -0500

    end of mod 5

--- react_pedro_mod5/src/App.js
+++ react_pedro_mod5/src/App.js
@@ -1,9 +1,68 @@
+// https://www.youtube.com/watch?v=f55qeKGgB_M&t=656s
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  // replaced by single line and object instead of simple string
+  // const deleteTask = (taskName) => {
+  //   const newTodoList = todoList.filter((task) => {
+  //     return task !== taskName;
+  //   });
+  //   setTodoList(newTodoList);
+  // };
+
+  // - reduced to shorter form
+  // const deleteTask = (taskName) => {
+  //   const newTodoList = todoList.filter((task) => {
+  //     if (task === taskName){
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   });
+  //   setTodoList(newTodoList);
+  // };
+
   return (
     <div className="App">
-      Hello World    
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div> 
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div className='task'>
+              <h1>{task.taskName}</h1>
+              <button 
+                className='button'
+                onClick={() => deleteTask(task.id)}
+                >
+                  DONE
+              </button>
+            </div>
+            );
+        })}
+      </div>   
     </div>
   );
 }
